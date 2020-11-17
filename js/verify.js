2bb139f9209005c40625195719826b9778212927
Joshua Butner <bynines@users.noreply.github.com>
2020-11-17 16:41:10 -0600

    Updated form to more consistently track submissions

--- js/verify.js
+++ js/verify.js
@@ -1,16 +1,24 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzPtz839njMg1c41Vqo_2QTiwO7ZjwOCDMhFqIgPM9XLuRJSHI/exec'
+const form = document.forms['verify-access-form']
+
 $(document).ready(function(){
 
-    $("#email_submit").click(function(e){
+    // Get and set IP data
+    $.getJSON("https://api.ipify.org?format=json", function(data) {
+        $("#ip").val(data.ip);
+    });
+
+    $("#submit").click(function(e){
 
         // Suppress POST
         e.preventDefault();
 
         // Retrieve the input field text
-        var filter = $("#email_address").val();
+        var filter = $("#email").val();
 
         // Process data, make available, run comparison
         var emAdd = [];
-        jQuery.get('/emadd.txt', function(data) {
+        $.get('/emadd.txt', function(data) {
             emAdd = data.split("\n");
             compareEntry(emAdd);
         });
@@ -28,12 +36,12 @@ $(document).ready(function(){
 
                 // Create an easily searchable list of data
                 $.each( data, function(i, val){
-                    $('#fauxdb').append('<li>' + val + '</li>');
+                    $('#fauxdb').append('<li>' + val.toLowerCase() + '</li>');
                 });
 
                 // Search the data
                 $("#fauxdb").find('*').filter(function() {
-                    if ( $(this).text() === filter ){
+                    if ( $(this).text() === filter.toLowerCase() ){
                       $('#error-message').hide();
                       found = true;
                       return false;
@@ -48,7 +56,8 @@ $(document).ready(function(){
             }
             else {
               $('#error-message').hide();
-              window.location.href = "https://25thheinzawards.com";
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => {window.location.href = "https://25thheinzawards.com"})
             }
         };
     });
